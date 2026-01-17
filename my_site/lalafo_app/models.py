da8664c7e9231a3654ca82e04c23da6e6ba2c4b6
medinaazymamatova-jpg <medinaazymamatova@gmail.com>
2026-01-17 16:05:37 +0600

    add files

--- my_site/lalafo_app/models.py
+++ my_site/lalafo_app/models.py
@@ -16,6 +16,7 @@ class Product(models.Model):
     category = models.ForeignKey(Category, on_delete= models.CASCADE)
     owner = models.CharField(max_length=32)
     created_date = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to='product_photo/', null=True, blank=True)
 
     def __str__(self):
         return f'{self.product_name} {self.price}'
--- my_site/lalafo_app/urls.py
+++ my_site/lalafo_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import CategoryViews
+from .views import CategoryViews, ProductViews
 
 urlpatterns = [
-    path('category/', CategoryViews.as_view())
+    path('category/', CategoryViews.as_view(), name='categories'),
+    path('product/', ProductViews.as_view(), name='product_list' )
 ]
